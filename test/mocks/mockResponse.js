8d4ca6cc3b8f368911805f23a988d70b01660df5
santiago.martincleto <santiago.martincleto@sngular.team>
2018-09-06 15:51:36 +0200

    Completing tests & fixings bugs

---
+++ test/mocks/mockResponse.js
@@ -0,0 +1,94 @@
+const mockResponse = {
+  'results': [
+    {
+      'name': {
+        'first': 'Mighty',
+        'last': 'Thor'
+      },
+      'dob': {
+        'age': 99
+      }
+    },
+    {
+      'name': {
+        'first': 'Peter',
+        'last': 'Parker'
+      },
+      'dob': {
+        'age': 26
+      }
+    },
+    {
+      'name': {
+        'first': 'Bruce',
+        'last': 'Banner'
+      },
+      'dob': {
+        'age': 35
+      }
+    },
+    {
+      'name': {
+        'first': 'Tony',
+        'last': 'Stark'
+      },
+      'dob': {
+        'age': 42
+      }
+    },
+    {
+      'name': {
+        'first': 'Bruce',
+        'last': 'Banner'
+      },
+      'dob': {
+        'age': 35
+      }
+    },
+    {
+      'name': {
+        'first': 'Charles',
+        'last': 'Xavier'
+      },
+      'dob': {
+        'age': 62
+      }
+    },
+    {
+      'name': {
+        'first': 'Natasha',
+        'last': 'Romanoff'
+      },
+      'dob': {
+        'age': 31
+      }
+    },
+    {
+      'name': {
+        'first': 'Matt',
+        'last': 'Murdock'
+      },
+      'dob': {
+        'age': 38
+      }
+    },
+    {
+      'name': {
+        'first': 'Matt',
+        'last': 'Murdock'
+      },
+      'dob': {
+        'age': 38
+      }
+    },
+    {
+      'name': {
+        'first': 'Ororo',
+        'last': 'Munroe'
+      },
+      'dob': {
+        'age': 27
+      }
+    },
+  ]
+};
